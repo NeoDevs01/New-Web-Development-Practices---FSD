eda58b2532789becfbd1864bf6171f5c99b01897
Riyaan's Devs <rsp749226@gmail.com>
2026-06-25 18:51:55 +0530

    Add files via upload

---
+++ T56 JS-3/index.js
@@ -0,0 +1,26 @@
+console.log("this is conditional statements");
+
+// let age = 16;
+// // let grace = 2;
+
+// if (age == 18) {
+//   console.log("you cann drive");
+// } else {
+//   console.log("you cannot drive");
+// }
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age / grace);
+// console.log(age * grace);
+// console.log(age % grace);
+// console.log(age ** grace);
+
+// Practice set 1:
+// let age = 100;
+// if ((age >= 10) && (age <= 20)) {
+//   console.log("True");
+// } else {
+//   console.log("false");
+// }
+
---
+++ T57 JS-4/index.js
@@ -0,0 +1,35 @@
+console.log("this is the loops in JS");
+
+// let a = 1;
+
+// for(let i = 0; i<100; i++){
+//     console.log(a+i);
+// }
+
+// let obj = {
+//   name: "Neo",
+//   role: "Programmer",
+//   company: "none"
+// }
+
+// for (const key of "neo") {
+//   console.log(key);
+// }
+
+// for (const element of obj) {
+//     console.log(obj)
+// }
+
+
+let i = 1;
+
+while(i<6){
+    console.log(i);
+    i++;
+}
+
+
+do {
+    console.log(i)
+    i++;
+} while (i<6);
