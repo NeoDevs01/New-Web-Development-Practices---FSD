531161ca7e93d25eb81c5824705527b555b7f18c
Riyaan's Devs <rsp749226@gmail.com>
2026-06-25 10:22:40 +0530

    Add files via upload

---
+++ T54/new.js
@@ -0,0 +1,3 @@
+console.log("Hello World!!!")
+console.log("Code is running...")
+console.log("Welcome to SIGMA Web Dev")
---
+++ T54/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World!!!")
+console.log("Code is running...")
+console.log("Welcome to SIGMA Batch OP!")
+
+
+alert("Welcome to JavaScript!")
+var a = prompt("Enter your number")
+
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("Computer is blasting")
+} else ("computer is not blasting")
+
+console.log("Your number is", + a)
+
+
+document.title = "Hey iam good"; // for inline css
+document.body.style.backgroundColor = "Red"
