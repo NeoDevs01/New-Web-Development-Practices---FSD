99a7e2ec24dcae9b540210d64c5c8804f30932c4
Riyaan's Devs <rsp749226@gmail.com>
2026-06-25 15:31:33 +0530

    Add files via upload

---
+++ T55 JS-2/script.js
@@ -0,0 +1,33 @@
+console.log("this is JS Variables")
+
+// var a = 10;
+// var b = 20;
+// var c = "tony";
+
+// console.log(a+b);
+
+// console.log(typeof a, typeof b, typeof c)
+
+let a = "neo";
+let b = 22;
+let c = 9.55;
+let x = undefined;
+let y = null;  // null type is obj in js for knowing reason refer stackoverflow
+const z = true;
+
+console.log(a,b,c,x,y,z);
+
+console.log(typeof a,typeof b,typeof c, typeof x,typeof y,typeof z);
+
+
+let o = {
+   name: "stark",
+   is_handsome: true,
+   salary: "100 Crores",
+   place: "banglore"
+}
+
+console.log(o);
+
+o.place = "Dubai";
+console.log(o)
